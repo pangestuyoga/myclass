4cd80eae10cadb30b79de7e91362aa3a35a70853
Yoga Pangestu <ypangestu976@gmail.com>
2026-03-10 12:46:10 +0700

    feat: Implement CustomPathGenerator for dynamic media path generation in the Media Library.

---
+++ app/MediaLibrary/CustomPathGenerator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\MediaLibrary;
+
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class CustomPathGenerator implements PathGenerator
+{
+    public function getPath(Media $media): string
+    {
+        $feature = $media->getCustomProperty('feature', 'misc');
+        $date = $media->getCustomProperty('date', now()->toDateString());
+        $docType = $media->getCustomProperty('doc_type');
+
+        $feature = str($feature)->slug();
+        $date = str($date);
+        $docType = $docType ? str($docType)->slug() : null;
+
+        $path = "{$feature}/";
+
+        if ($docType) {
+            $path .= "{$docType}/";
+        }
+
+        $path .= "{$date}/";
+
+        return $path;
+    }
+
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getPath($media).'conversions/';
+    }
+
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getPath($media).'responsive/';
+    }
+}
